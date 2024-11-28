eb24a56203365c633e4ed5212b8c5c8bb5606d1b
unknown <https://github.com/hassan19071>
2024-11-28 17:23:33 +0200

    some updates

--- js/main.js
+++ js/main.js
@@ -2,7 +2,32 @@
 let mobileLinks = document.querySelector(".navbar .mobile-links");
 let menuBtn = document.querySelector(".navbar .menu-icon");
 
-menuBtn.addEventListener("click",()=>{
-    mobileLinks.classList.toggle("active");
-    menuBtn.classList.toggle('active');
-})
+menuBtn.addEventListener("click", () => {
+  mobileLinks.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+});
+
+// scroll to top;
+let scrollToBtn = document.querySelector(".scroll-to-top");
+
+scrollToBtn.addEventListener("click", () => {
+  scrollTo({
+    top: "0",
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToBtn.classList.add("active");
+  } else {
+    scrollToBtn.classList.remove("active");
+  }
+});
+
+// laoding page;
+let loadingPage = document.querySelector(".loading-section");
+
+window.onload = () => {
+  loadingPage.classList.add("hide");
+};
